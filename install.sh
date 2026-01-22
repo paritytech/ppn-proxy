4f5b42e6318a4c8ff2d67163701c25d2bdb2e6d4
Charles Hetterich <hetterich.charles@gmail.com>
2026-01-22 19:25:38 +0000

    removed specific install. instructions from proxy install script

--- install.sh
+++ install.sh
@@ -1,16 +1,6 @@
-# Check for cargo, install if missing
-if ! command -v cargo &> /dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-    source "$HOME/.cargo/env"
-fi
-
-# Check for zombie-cli, install if missing
-if ! command -v zombie &> /dev/null; then
-    cargo install zombie-cli
-fi
-
 # Fetch and generate product-preview-net configs
 git clone --depth 1 git@github.com:paritytech/product-preview-net.git /tmp/ppn && \
+bash /tmp/ppn/install.sh && \
 bash /tmp/ppn/fetch.sh && \
 bash /tmp/ppn/generate.sh && \
 cp /tmp/ppn/zombienet-configs/local-dev.toml ./bin/ && \
