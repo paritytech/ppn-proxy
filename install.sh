cc3d878757c7399ba34f0467b0981bab197b8bb9
Charles Hetterich <hetterich.charles@gmail.com>
2026-01-26 23:57:05 -0500

    updated install script to take optional `BRANCH` input

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
+# Optional branch argument
+BRANCH="${1:-}"
+BRANCH_FLAG=""
+if [ -n "$BRANCH" ]; then
+  BRANCH_FLAG="--branch $BRANCH"
+fi
+
 # Fetch and generate product-preview-net configs
-git clone --depth 1 git@github.com:paritytech/product-preview-net.git /tmp/ppn && \
+rm -rf /tmp/ppn
+git clone --depth 1 $BRANCH_FLAG git@github.com:paritytech/product-preview-net.git /tmp/ppn && \
 source /tmp/ppn/scripts/install.sh && \
 bash /tmp/ppn/scripts/fetch.sh && \
 bash /tmp/ppn/scripts/generate.sh && \
 cp /tmp/ppn/zombienet-configs/local-dev.toml ./bin/ && \
 rm -rf /tmp/ppn
-
-echo "
-To start the zombienet:
-
-BIN=\$(pwd)/bin zombie-cli spawn -p native ./bin/local-dev.toml
-"
