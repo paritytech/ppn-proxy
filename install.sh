090909cfe92138223deaace11d1982bd9e3df98f
Charles Hetterich <hetterich.charles@gmail.com>
2026-02-03 14:50:06 +0000

    changed clone to use https instead of ssh to avoid needing ssh auth

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 # Fetch and generate product-preview-net configs
 rm -rf /tmp/ppn
-git clone --depth 1 $BRANCH_FLAG git@github.com:paritytech/product-preview-net.git /tmp/ppn && \
+git clone --depth 1 $BRANCH_FLAG https://github.com/paritytech/product-preview-net.git /tmp/ppn && \
 source /tmp/ppn/scripts/install.sh && \
 bash /tmp/ppn/scripts/fetch.sh && \
 bash /tmp/ppn/scripts/generate.sh && \
